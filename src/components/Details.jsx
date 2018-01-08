16576e63d9ed70c425eb83b879693b09344b9f71
Johan <34781723+johandjoz@users.noreply.github.com>
2018-01-08 21:38:45 +0100

    Update Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -148,7 +148,7 @@ class Details extends Component {
                     <Row className="r2">
                         <Col md={6} className="max-width">
                             <h1 style={{color:'green'}}>Public</h1>
-                            <h3>Zen Address</h3>
+                            <h3>ZCL Address</h3>
                             <div>
                                 <QRCode
                                     bgColor="#FFFFFF"
